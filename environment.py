e19a47ea8167d04f960f3a382180b5a6dd81d238
BuildTools <unconfigured@null.spigotmc.org>
2026-07-21 13:48:39 -0500

    Fixed major error in handling the can_jump variable

--- environment.py
+++ environment.py
@@ -2,6 +2,7 @@ import pygame
 import pymunk
 import env_functions as fcs
 
+# General settings
 SCREEN_DIMENSIONS = (300, 600)
 GRAVITY = 450
 BALL_TYPE = 0
@@ -10,11 +11,15 @@ SPIKE_TYPE = 2
 GOAL_TYPE = 3
 CONVEYOR_TYPE = 4
 
-
+# Pygame and pymunk initialization
 pygame.init()
 screen = pygame.display.set_mode(SCREEN_DIMENSIONS)
 clock = pygame.time.Clock()
 running = True
+space = pymunk.Space()
+space.gravity = 0.0, -GRAVITY
+
+# Level and state storage
 can_jump = False
 all_objects = []
 platforms = []
@@ -22,18 +27,23 @@ spikes = []
 conveyors = []
 goal = []
 
-space = pymunk.Space()
-space.gravity = 0.0, -GRAVITY
-
+# Ball initialization
 ball_body = pymunk.Body(mass = 10, moment = 1, body_type=pymunk.Body.DYNAMIC)
 ball_body.position = (150, 600)
 ball_shape = pymunk.Circle(ball_body, 10)
 ball_shape.collision_type = BALL_TYPE
 space.add(ball_body, ball_shape)
 
+# World border initialization
+border_body = space.static_body
+bottom_shape = pymunk.Segment(border_body, (-SCREEN_DIMENSIONS[0], -60), (SCREEN_DIMENSIONS[0] * 2, -SCREEN_DIMENSIONS[0]), radius=10)
+bottom_shape.collision_type = SPIKE_TYPE
+space.add(bottom_shape)
+
+# Level Creation
 platforms.append(fcs.add_platform(space, 150, 500, 0.25))
 platforms.append(fcs.add_platform(space, 0, 300, -0.5))
-spikes.extend(fcs.add_platform_spike(space, platforms[0][1], num_spikes=2))
+spikes.extend(fcs.add_platform_spike(space, platforms[0][1], higher=True, num_spikes=2))
 spikes.append(fcs.add_platform_spike(space, platforms[1][1]))
 conveyors.append(fcs.add_conveyor(space, 200, 150, 100, 150))
 spikes.extend(fcs.add_conveyor_spike(space, conveyors[0][1], num_spikes=2))
@@ -44,6 +54,7 @@ all_objects.extend(spikes)
 all_objects.extend(conveyors)
 all_objects.extend(goal)
 
+# Collision handlers
 def mark_visited(objects, shape: pymunk.Shape):
     for object in objects:
         if (object[1] == shape):
@@ -53,6 +64,8 @@ def mark_visited(objects, shape: pymunk.Shape):
 def reset_world(space: pymunk.Space):
     ball_body.position = (150, 600)
     ball_body.velocity = (0, 0)
+    global can_jump
+    can_jump = False
     for conveyor in conveyors:
         conveyor[0].position = conveyor[0].origin
         conveyor[0].velocity = (0, 0)
@@ -62,21 +75,28 @@ def reset_world(space: pymunk.Space):
             spike[0].velocity = (0, 0)
     for platform in platforms:
         platform[2] = False
+    print(can_jump)
 
 def end_fail(arbiter, space, data):
     reset_world(space)
+    print('end_fail')
 def end_win(arbiter, space, data):
     reset_world(space)
+    print('end_win')
 
-def flip_jump_state(arbiter, space, data):
+def begin_platform(arbiter, space, data):
     global can_jump
-    can_jump = not can_jump
+    can_jump = True
     _, platform_shape = arbiter.shapes
     mark_visited(platforms, platform_shape)
+    print('flip_jump_state')
+def separate_platform(arbiter, space, data):
+    global can_jump
+    can_jump = False
 
 def move_conveyor(arbiter, space, data):
     global can_jump
-    can_jump = not can_jump
+    can_jump = True
     ball_shape, conveyor_shape = arbiter.shapes
 
     mark_visited(conveyors, conveyor_shape)
@@ -93,16 +113,25 @@ def move_conveyor(arbiter, space, data):
 def check_conveyor_state(arbiter, space, data):
     _, conveyor_shape = arbiter.shapes
     if conveyor_shape.body.position.x * conveyor_shape.body.direction > conveyor_shape.body.target * conveyor_shape.body.direction:
-        conveyor_shape.body.velocity = (0, 0)
-        for spike in conveyor_shape.body.spikes:
-            spike[0].velocity = (0, 0)
+        stop_movement(arbiter, space, data)
+        global can_jump
+        can_jump = True
+def stop_movement(arbiter, space, data):
+    global can_jump
+    can_jump = False 
+    _, conveyor_shape = arbiter.shapes
+    conveyor_shape.body.velocity = (0, 0)
+    for spike in conveyor_shape.body.spikes:
+        spike[0].velocity = (0, 0)
 
-space.on_collision(BALL_TYPE, STATIC_TERRAIN_TYPE, begin=flip_jump_state, separate=flip_jump_state)
+space.on_collision(BALL_TYPE, STATIC_TERRAIN_TYPE, begin=begin_platform, separate=separate_platform)
+space.on_collision(BALL_TYPE, CONVEYOR_TYPE, begin=move_conveyor, pre_solve=check_conveyor_state, separate=stop_movement)
 space.on_collision(BALL_TYPE, SPIKE_TYPE, begin=end_fail)
-space.on_collision(BALL_TYPE, CONVEYOR_TYPE, begin=move_conveyor, pre_solve=check_conveyor_state, separate=flip_jump_state)
 space.on_collision(BALL_TYPE, GOAL_TYPE, begin=end_win)
 
+# Main game loop
 while running:
+    # Event handlers
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -111,7 +140,7 @@ while running:
     
     screen.fill('white')
 
-    # render
+    # Rendering
     pygame.draw.circle(screen, pygame.Color('black'), 
                        (int(ball_shape.body.position.x), 
                        int(fcs.flipy(ball_shape.body.position.y))), 
@@ -130,9 +159,14 @@ while running:
         start = goal[0][0].local_to_world(segment.a)
         end = goal[0][0].local_to_world(segment.b)
         pygame.draw.line(screen, pygame.Color('black'), (start.x, fcs.flipy(start.y)), (end.x, fcs.flipy(end.y)), width=int(segment.radius*2))
+    
+    # Advance physics engine
     dt = 1.0 / 60.0
     for x in range(1):
         space.step(dt)
+    
+    # Reset when ball leaves screen
+    # Theoretically should never run due to the border, but just in case something weird happens, still might be useful. 
     if ball_body.position[1] < -50:
         reset_world(space)
     pygame.display.flip()
